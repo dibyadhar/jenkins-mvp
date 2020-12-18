80f04cbbd0868204d481a6f72fb8dc3114431ed9
Dibya Dhar <dibyaofficial524@gmail.com>
2020-12-18 01:57:34 +0000

    Update logic to create sample archival file

--- python/hello-world.py
+++ python/hello-world.py
@@ -6,5 +6,6 @@ if os.path.exists("software.txt"):
   os.remove("software.txt")
 else:
   print("The file does not exist")
-  f = open("software.txt", "x")
-  f.close
+  
+f = open("software.txt", "x")
+f.close
