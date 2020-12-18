e35ce7984e0eef0f2f194ff71702db73d8408999
Dibya Dhar <dibyaofficial524@gmail.com>
2020-12-18 01:51:38 +0000

    Add logic to  remove previous archive file

--- python/hello-world.py
+++ python/hello-world.py
@@ -1,3 +1,10 @@
+import os
+
 print("Hello Jenkins")
-f = open("software.txt", "x")
-f.close
+
+if os.path.exists("software.txt"):
+  os.remove("demofile.txt")
+else:
+  print("The file does not exist")
+  f = open("software.txt", "x")
+  f.close
